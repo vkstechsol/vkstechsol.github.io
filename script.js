c7dbbfd61aab11d6dd6097f1afb9b5529256b10e
vkstechsol <109616740+vkstechsol@users.noreply.github.com>
2023-04-09 11:14:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const words = [" 1- Chatty - धबातूनी ", " 2- Kinship - संबंध, रिश्ता", " 3- Magnanimous - उदार, दयालु, ", " 4- Nefarious - दुर्जन, दुष्ट,", " 5- Stingy- कंजूस ", "6- Fearless -निडर ", " 7- Slim - दुबला-पतला, ", " 8- Diligent - मेहनती", " 9- Candid - मासूम"," 10-Docile - विनम्र"," 11- Silently - दिल ही दिल में", " 12- Get Annoyed - नाराज़ होना", " 13- Back out - मुकर जाना", " 14 - Make promises - वादे करना:, " 15- Guide - मार्गदर्शक" ];
+const words = [" 1- Chatty - धबातूनी ", " 2- Kinship - संबंध, रिश्ता", " 3- Magnanimous - उदार, दयालु, ", " 4- Nefarious - दुर्जन, दुष्ट,", " 5- Stingy- कंजूस ", "6- Fearless -निडर ", " 7- Slim - दुबला-पतला, ", " 8- Diligent - मेहनती", " 9- Candid - मासूम"," 10-Docile - विनम्र"," 11- Silently - दिल ही दिल में", " 12- Get Annoyed - नाराज़ होना", " 13- Back out - मुकर जाना", " 14 - Make promises - वादे करना:", " 15- Guide - मार्गदर्शक" ];
 
 let index = 0;
 const wordElement = document.getElementById("word");
